6ddb29ee51e93395064236ebe5eb56b31fd3b212
Tony wong <tywong11@gmail.com>
2024-02-20 21:20:58 -0700

    routing funcationally

--- frontend/my-app/src/app/app.routes.ts
+++ frontend/my-app/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
-    { path:'', component:LoginComponent}    
+    { path:'', component:HomeComponent} ,  
+    { path:'login', component:LoginComponent},
+    { path:'register', component:RegisterComponent},
+    { path: 'dashboard', component:DashboardComponent}
 ];
