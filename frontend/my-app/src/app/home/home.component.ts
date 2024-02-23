ff1eb57819c32121062e5a3aa72c8fd7b3c57f78
Tony wong <tywong11@gmail.com>
2024-02-22 19:52:24 -0700

    home css changes

--- frontend/my-app/src/app/home/home.component.ts
+++ frontend/my-app/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink,  MatSlideToggleModule],
+  imports: [RouterLink,  MatSlideToggleModule, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
